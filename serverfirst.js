1d28971a9c79f48f77c46a9b977e534aa1b8b3f8
rahulsai121 <pasam.rahul1234@gmail.com>
2024-02-16 17:10:31 +0530

    serversecond

--- serverfirst.js
+++ serverfirst.js
@@ -1,8 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Rahul');
-    
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>first Page</title></head>')
+    if((req.url==='/home')){
+
+        res.write('<body><h1>Welcome to Home</h1></body>')
+    }
+    else if((req.url==='/about')){
+
+        res.write('<body><h1>Welcome to About Us Page</h1></body>')
+    }
+    else if((req.url==='/node')){
+
+        res.write('<body><h1>Welcome to My Node Js Project</h1></body>')
+    }
+    res.write('</html>')
 });
 
-server.listen(4000);
+server.listen(4004);
