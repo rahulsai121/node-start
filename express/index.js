cbf1d648b67a615e09baafe69f86c8639d3b2553
rahulsai121 <pasam.rahul1234@gmail.com>
2024-03-28 14:40:59 +0530

    second

--- express/index.js
+++ express/index.js
@@ -1,15 +1,21 @@
 
 
-const express=require('express')
+const express=require('express');
 
-const app=express()
+const bodyParser = require('body-parser');
 
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    next();
+const app=express();
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res)=>{
+    res.send('<form action="/product" method="POST">Product name:<input type="text" name="title">Product size:<input type="number" name="size"><button type="submit">submit</button></form>')
+})
+app.post('/product',(req,res)=>{
+    console.log(req.body)
+    res.redirect('/');
 })
 app.use((req,res,next)=>{
-    console.log('another middleware')
     res.send('<h1>Hello from express</h1>')
 })
 
