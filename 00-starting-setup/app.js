0b315a84de6b9fffbcdfe2a052cc9f8ec9b697e9
rahulsai121 <pasam.rahul1234@gmail.com>
2024-04-23 14:12:53 +0530

    seq1

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
+const sequelize=require('./util/database')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,4 +25,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+.then(result=>{
+    console.log(result);
+    app.listen(3000);
+
+})
+.catch(err=>console.log(err))
+
--- 00-starting-setup/models/product.js
+++ 00-starting-setup/models/product.js
@@ -1,26 +1,30 @@
-const db=require('../util/database')
+const Sequelize=require('sequelize')
 
-module.exports = class Product {
-  constructor(id,title, imageUrl, description, price) {
-    this.id=id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const sequelize=require('../util/database')
 
-  save() {
-  return  db.execute('INSERT INTO product(title,price,imageUrl,description) VALUES (?,?,?,?)',
-  [this.title, this.price, this.imageUrl, this.description])
+const Product=sequelize.define('product',{
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true
+  },
+  title:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  price:{
+    type:Sequelize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allowNull:false
   }
+})
 
-  static deleteById(id){
-    db.execute('DELETE FROM product WHERE product.id=?',[id])
-  }
-  static fetchAll() {
-    return db.execute('SELECT * FROM product')
-  }
-  static findById(id){
-  return db.execute('SELECT * FROM product where product.id=?',[id]) 
-  }
-};
+module.exports=Product;
