d596687479f742eb7ef31c96c1a0ac53eb16ec15
rahulsai121 <pasam.rahul1234@gmail.com>
2024-05-15 12:30:36 +0530

    associ

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -7,6 +7,10 @@ const errorController = require('./controllers/error');
 
 const sequelize=require('./util/database')
 
+const Product=require('./models/product')
+
+const User=require('./models/user')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,6 +20,14 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
+app.use((req,res,next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user=user
+        next()
+    })
+    .catch(err=>console.log(err))
+})
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,11 +37,23 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User)
+User.hasMany(Product)
+
 sequelize.sync()
 .then(result=>{
-    console.log(result);
+    return User.findByPk(1)
+    //console.log(result);
+    
+})
+.then (user=>{
+    if(!user){
+        return User.create({name:'Max',email:'text@text.com'})
+    }
+    return user
+})
+.then(user=>{
     app.listen(3000);
-
 })
 .catch(err=>console.log(err))
 
--- 00-starting-setup/controllers/admin.js
+++ 00-starting-setup/controllers/admin.js
@@ -13,11 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title:title,
     price:price,
     imageUrl:imageUrl,
     description:description
+
   })
   .then((result)=>{
     console.log(result)
@@ -33,7 +34,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/')
   }
   const prodId=req.params.productId;
-  Product.findById(prodId,product=>{
+  req.user.getProducts({where:{id:prodId}})
+  .then(products=>{
+    const product=products[0]
     if(!product){
       return res.redirect('/');
       
