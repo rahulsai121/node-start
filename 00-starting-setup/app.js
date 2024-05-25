1e5be4d2f8f5384a3ca27d84367052ac0b927d0d
rahulsai121 <pasam.rahul1234@gmail.com>
2024-05-25 11:06:48 +0530

    kk

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -11,6 +11,11 @@ const Product=require('./models/product')
 
 const User=require('./models/user')
 
+const Cart=require('./models/cart')
+
+const CartItem=require('./models/cart-item')
+
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -39,6 +44,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User)
 User.hasMany(Product)
+User.hasOne(Cart);
+Cart.belongsTo(User)
+Cart.belongsToMany(Product,{through:CartItem})
+Product.belongsToMany(Cart,{through:CartItem})
 
 sequelize.sync()
 .then(result=>{
